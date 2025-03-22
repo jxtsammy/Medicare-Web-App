3230030813e59b910b93100eec2d0d6e15bcce28
Your Name <you@example.com>
2025-03-22 17:30:20 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Medicare from './Patients Flow/Homepage/Home'
+//import Login from './Provider Flow/Provider Login/ProviderLogin'
+//import Resgister from './Provider Flow/Provider Register/ProviderRegister'
+// import Dashboard from './Provider Flow/Provider Dashboard/Dashboard'
+//import Appointment from './Provider Flow/Appointments/Appointment'
+import Chat from './Provider Flow/Provider Chat/Chat'
 function App() {
-
   return (
     <>
-      <Medicare />
+      <Chat />
     </>
   )
 }
