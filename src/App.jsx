8937b1547e06d642978ebb21db3b242df89fd1df
Your Name <you@example.com>
2025-03-23 15:17:12 +0000

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Provider Flow/Provider Login/ProviderLogin";
-import Register from "./Provider Flow/Provider Register/ProviderRegister";
+import ProviderLogin from "./Provider Flow/Provider Login/ProviderLogin";
+import ProviderRegister from "./Provider Flow/Provider Register/ProviderRegister";
 import Dashboard from "./Provider Flow/Provider Dashboard/Dashboard";
 import Appointment from "./Provider Flow/Appointments/Appointment";
 import Chat from "./Provider Flow/Provider Chat/Chat";
 import Profile from "./Provider Flow/Profile Settings/Profile-settings";
 import ProfileBio from "./Provider Flow/Edit Profile/EditProfile";
+import Home from './Patient Flow/HomePage/LandingPage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Home />} />
+        <Route path="/provider-login" element={<ProviderLogin />} />
+        <Route path="/provider-register" element={<ProviderRegister />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/appointments" element={<Appointment />} />
         <Route path="/chat" element={<Chat />} />
